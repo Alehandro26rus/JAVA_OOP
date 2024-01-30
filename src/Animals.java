ad878d7562f235d12c4d66107f82f0bf9e2815f4
Alehandro_26rus <skash96@yandex.ru>
2024-01-30 16:29:51 +0300

    Initial commit

---
+++ src/Animals.java
@@ -0,0 +1,32 @@
+import java.time.LocalDate;
+
+public class Animals {
+
+    protected String name;
+
+    public Animals(String name){
+        this.name = name;
+    }
+
+    public String getType(){
+        return getClass().getSimpleName();
+    }
+
+    protected void toGo(){
+        System.out.println(getType() + " пошел ");
+    }
+
+    protected void fly(){
+        System.out.println(getType() + " полетел ");
+    }
+
+    protected void swim(){
+        System.out.println(getType() + " поплыл ");
+    }
+
+    public void justDoIt(){
+        toGo();
+        fly();
+        swim();
+    }
+}
