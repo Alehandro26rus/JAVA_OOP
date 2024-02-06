3bc59ce7127258839e2a9393f8a8e89df0825122
Alehandro26rus <144227710+Alehandro26rus@users.noreply.github.com>
2024-02-06 19:01:41 +0300

    Update Guppy.java

--- src/animals/Guppy.java
+++ src/animals/Guppy.java
@@ -1,2 +1,15 @@
-package animals;public class Guppy {
+package org.example.animals;
+
+import org.example.interfaces.Swimable;
+
+//гуппи
+public class Guppy extends Animal implements Swimable {
+    public Guppy(String name) {
+        super.name = name;
+    }
+
+    @Override
+    public double swim() {
+        return 10;
+    }
 }
